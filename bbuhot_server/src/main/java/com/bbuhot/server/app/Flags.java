b5d6ac4604c1debb7e7cd3055866c173e0758861
yh_victor <yhvictor@users.noreply.github.com>
2018-12-08 17:42:30 -0600

    Make intellij works better.
    If intellij failed to build by itself, try:
    Settings -> Build, Execution, Deployment
      -> Build Tools -> Gradle -> Runner
      -> Delegate IDE build/run actions to gradle.

--- bbuhot_server/src/main/java/com/bbuhot/server/app/Flags.java
+++ bbuhot_server/src/main/java/com/bbuhot/server/app/Flags.java
@@ -31,7 +31,7 @@ public class Flags {
 
   public static class Database {
 
-    private final String url = "jdbc:mysql://127.0.0.1:3306/ultrax";
+    private final String url = "jdbc:mysql://192.168.31.23:3306/ultrax";
     private final String user = "root";
     private final String password = "bbuhot";
     private final String tablePrefix = "pre_";
