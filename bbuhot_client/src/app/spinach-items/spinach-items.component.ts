d4539ea04bd806a20c10ce369e777f068388e721
yh_victor <yhvictor@users.noreply.github.com>
2018-12-17 22:11:43 -0800

    add client code (#13)
    * add client code
    
    * Add end of line.

---
+++ bbuhot_client/src/app/spinach-items/spinach-items.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-spinach-items',
+  templateUrl: './spinach-items.component.html',
+  styleUrls: ['./spinach-items.component.css']
+})
+export class SpinachItemsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
