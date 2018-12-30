cc6a4528cb37f9de3b52c45a9c5f5ce5a905cee4
CinCommon <452495748@qq.com>
2018-12-31 02:07:26 +0800

    添加JS代码, 控制一些逻辑 1. 胜利的右上角红色Tag 2. 结束后赔率Label的颜色变灰 3. Input框没有输入的时候确定按钮是灰色的 4. 调整样式
    待处理:
    1. 未选择盘口的提示Label的显示条件?
    2. 卡片选中后, 下方的绿色提示条的逻辑?
    3. 入力框合法性检查.
    4. 点击卡片的事件处理逻辑.

---
+++ bbuhot_client/src/models/Team.ts
@@ -0,0 +1,9 @@
+export default class Team {
+  teamId: number;
+  teamName: string;
+  constructor(teamId: number,
+    teamName: string) {
+    this.teamId = teamId;
+    this.teamName = teamName;
+  }
+}
---
+++ bbuhot_client/src/models/index.ts
@@ -0,0 +1,9 @@
+import Quiz from './Quiz';
+import Team from './Team';
+import TeamRatio from './TeamRatio';
+
+export {
+  Quiz,
+  Team,
+  TeamRatio,
+};
