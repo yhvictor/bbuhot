beac126fae36948f2031d0129f6617a5e26f39c6
Insomnia <282132315@qq.com>
2019-01-11 22:02:35 +0800

    fix code review's suggestion except admin manager css

--- bbuhot_client/src/app/admin/main/admin-main/models/sider.models.ts
+++ bbuhot_client/src/app/admin/main/admin-main/models/sider.models.ts
@@ -1,7 +1,3 @@
 export class SiderModels {
-  constructor(private title: string, private icon: string, private path: string) {
-    this.title = title;
-    this.icon = icon;
-    this.path = path;
-  }
+  constructor(private title: string, private icon: string, private path: string) {}
 }
