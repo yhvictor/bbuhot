06dd7338b4fced45c85c5a1ea57cea80f0df0009
luciusgone <luciusgone@gmail.com>
2019-01-16 14:59:06 +0800

    [WIP] add more test cases for listing games

--- bbuhot_server/src/test/java/com/bbuhot/server/service/GameListingServiceTest.java
+++ bbuhot_server/src/test/java/com/bbuhot/server/service/GameListingServiceTest.java
@@ -36,7 +36,7 @@ public class GameListingServiceTest {
     Logger.getLogger("org.hibernate").setLevel(Level.SEVERE);
     TestServiceComponent.getInstance().inject(this);
 
-    // create admin user
+    // create user
     UserEntity user = new UserEntity();
     user.setUid(1);
     user.setUsername("admin");
@@ -46,17 +46,39 @@ public class GameListingServiceTest {
 
     // create games
     // draft game
+    createGame(1, /* visible= */false, GameEntityStatus.DRAFT);
+
+    // published game
+    GameEntity game = createGame(2, /* visible= */false, GameEntityStatus.PUBLISHED);
+    createBet(1, user.getUid(), game.getId(), BetEntityStatus.UNSETTLED);
+    game = createGame(3, /* visible= */true, GameEntityStatus.PUBLISHED);
+    createBet(2, user.getUid(), game.getId(), BetEntityStatus.UNSETTLED);
+
+    // settled game
+    game = createGame(4, /* visible= */true, GameEntityStatus.SETTLED);
+    createBet(3, user.getUid(), game.getId(), BetEntityStatus.SETTLED);
+
+    // cancelled game
+    game = createGame(5, /* visible= */true, GameEntityStatus.CANCELLED);
+    createBet(4, user.getUid(), game.getId(), BetEntityStatus.CANCELLED);
+  }
+
+  private GameEntity createGame(int id, boolean visible, GameEntityStatus status) {
     GameEntity game = new GameEntity();
-    game.setId(1);
+    game.setId(id);
     game.setName("Ti8 Grand final LGD vs OG");
     game.setDescription("Ti8 Grand final LGD vs OG");
-    game.setNormalUserVisible(false);
-    game.setStatus(GameEntityStatus.DRAFT.value);
+    game.setNormalUserVisible(visible);
+    game.setStatus(status.value);
     game.setBetOptionLimit(1);
     game.setBetAmountLowest(100);
     game.setBetAmountHighest(400);
     game.setEndTimeMs(new Timestamp(1535126400000L));
-    game.setWinningBetOption(-2);
+    if (status == GameEntityStatus.SETTLED) {
+      game.setWinningBetOption(0);
+    } else {
+      game.setWinningBetOption(-2);
+    }
     BettingOptionEntity option1 = new BettingOptionEntity();
     option1.setName("LGD win");
     option1.setOdds(2000000);
@@ -66,36 +88,18 @@ public class GameListingServiceTest {
     option2.setOdds(8000000);
     game.getBettingOptionEntities().add(option2);
     gameQueries.save(game);
-    // published game
-    GameEntity game2 = new GameEntity();
-    game2.setId(2);
-    game2.setName("Ti8 Grand final LGD vs OG BO5");
-    game2.setDescription("Ti8 Grand final LGD vs OG BO5");
-    game2.setNormalUserVisible(true);
-    game2.setStatus(GameEntityStatus.PUBLISHED.value);
-    game2.setBetOptionLimit(1);
-    game2.setBetAmountLowest(100);
-    game2.setBetAmountHighest(400);
-    game2.setEndTimeMs(new Timestamp(1535126400000L));
-    game2.setWinningBetOption(-2);
-    BettingOptionEntity option3 = new BettingOptionEntity();
-    option3.setName("LGD win");
-    option3.setOdds(2000000);
-    game2.getBettingOptionEntities().add(option3);
-    BettingOptionEntity option4 = new BettingOptionEntity();
-    option4.setName("OG win");
-    option4.setOdds(8000000);
-    game2.getBettingOptionEntities().add(option4);
-    gameQueries.save(game2);
 
-    // create bets
+    return game;
+  }
+
+  private void createBet(int id, int uid, int gameId, BetEntityStatus status) {
     BetEntity bet = new BetEntity();
-    bet.setId(1);
-    bet.setUid(user.getUid());
-    bet.setGameId(game2.getId());
+    bet.setId(id);
+    bet.setUid(uid);
+    bet.setGameId(gameId);
     bet.setBettingOptionId(0);
     bet.setBetAmount(100);
-    bet.setStatus(BetEntityStatus.UNSETTLED.value);
+    bet.setStatus(status.value);
     bet.setCreatedAt(new Timestamp(1535126300000L));
     betQueries.save(bet);
   }
@@ -109,8 +113,7 @@ public class GameListingServiceTest {
   }
 
   @Test
-  public void testGameListing() {
-    // listing draft games as admin
+  public void testAdminDraftGameListing() {
     ListGameRequest listGameRequest =
       TestMessageUtil.getResourcesAsMessage(
           ListGameRequest.getDefaultInstance(),
@@ -123,33 +126,101 @@ public class GameListingServiceTest {
 
     ListGameReply listGameReply = listGameService.callProtobufServiceImpl(listGameRequest);
     assertEquals(listGameReplyExpected, listGameReply);
+  }
 
-    // listing published games as admin
-    listGameRequest =
+  @Test
+  public void testAdminPublishedGameListing() {
+    ListGameRequest listGameRequest =
       TestMessageUtil.getResourcesAsMessage(
           ListGameRequest.getDefaultInstance(),
-          "com/bbuhot/server/service/admin_list_game_request.json");
+          "com/bbuhot/server/service/admin_list_published_game_request.json");
 
-    listGameReplyExpected =
+    ListGameReply listGameReplyExpected =
       TestMessageUtil.getResourcesAsMessage(
           ListGameReply.getDefaultInstance(),
-          "com/bbuhot/server/service/admin_list_game_reply.json");
+          "com/bbuhot/server/service/admin_list_published_game_reply.json");
 
-    listGameReply = listGameService.callProtobufServiceImpl(listGameRequest);
+    ListGameReply listGameReply = listGameService.callProtobufServiceImpl(listGameRequest);
     assertEquals(listGameReplyExpected, listGameReply);
+  }
 
-    // listing published game as normal user
-    listGameRequest =
+  @Test
+  public void testAdminSettledGameListing() {
+    ListGameRequest listGameRequest =
       TestMessageUtil.getResourcesAsMessage(
           ListGameRequest.getDefaultInstance(),
-          "com/bbuhot/server/service/user_list_game_request.json");
+          "com/bbuhot/server/service/admin_list_settled_game_request.json");
 
-    listGameReplyExpected =
+    ListGameReply listGameReplyExpected =
       TestMessageUtil.getResourcesAsMessage(
           ListGameReply.getDefaultInstance(),
-          "com/bbuhot/server/service/user_list_game_reply.json");
+          "com/bbuhot/server/service/admin_list_settled_game_reply.json");
 
-    listGameReply = listGameService.callProtobufServiceImpl(listGameRequest);
+    ListGameReply listGameReply = listGameService.callProtobufServiceImpl(listGameRequest);
+    assertEquals(listGameReplyExpected, listGameReply);
+  }
+
+  @Test
+  public void testAdminCancelledGameListing() {
+    ListGameRequest listGameRequest =
+      TestMessageUtil.getResourcesAsMessage(
+          ListGameRequest.getDefaultInstance(),
+          "com/bbuhot/server/service/admin_list_cancelled_game_request.json");
+
+    ListGameReply listGameReplyExpected =
+      TestMessageUtil.getResourcesAsMessage(
+          ListGameReply.getDefaultInstance(),
+          "com/bbuhot/server/service/admin_list_cancelled_game_reply.json");
+
+    ListGameReply listGameReply = listGameService.callProtobufServiceImpl(listGameRequest);
+    assertEquals(listGameReplyExpected, listGameReply);
+  }
+
+  @Test
+  public void testUserPublisedGameListing() {
+    ListGameRequest listGameRequest =
+      TestMessageUtil.getResourcesAsMessage(
+          ListGameRequest.getDefaultInstance(),
+          "com/bbuhot/server/service/user_list_published_game_request.json");
+
+    ListGameReply listGameReplyExpected =
+      TestMessageUtil.getResourcesAsMessage(
+          ListGameReply.getDefaultInstance(),
+          "com/bbuhot/server/service/user_list_published_game_reply.json");
+
+    ListGameReply listGameReply = listGameService.callProtobufServiceImpl(listGameRequest);
+    assertEquals(listGameReplyExpected, listGameReply);
+  }
+
+  @Test
+  public void testUserSettledGameListing() {
+    ListGameRequest listGameRequest =
+      TestMessageUtil.getResourcesAsMessage(
+          ListGameRequest.getDefaultInstance(),
+          "com/bbuhot/server/service/user_list_settled_game_request.json");
+
+    ListGameReply listGameReplyExpected =
+      TestMessageUtil.getResourcesAsMessage(
+          ListGameReply.getDefaultInstance(),
+          "com/bbuhot/server/service/user_list_settled_game_reply.json");
+
+    ListGameReply listGameReply = listGameService.callProtobufServiceImpl(listGameRequest);
+    assertEquals(listGameReplyExpected, listGameReply);
+  }
+
+  @Test
+  public void testUserCancelledGameListing() {
+    ListGameRequest listGameRequest =
+      TestMessageUtil.getResourcesAsMessage(
+          ListGameRequest.getDefaultInstance(),
+          "com/bbuhot/server/service/user_list_cancelled_game_request.json");
+
+    ListGameReply listGameReplyExpected =
+      TestMessageUtil.getResourcesAsMessage(
+          ListGameReply.getDefaultInstance(),
+          "com/bbuhot/server/service/user_list_cancelled_game_reply.json");
+
+    ListGameReply listGameReply = listGameService.callProtobufServiceImpl(listGameRequest);
     assertEquals(listGameReplyExpected, listGameReply);
   }
 }
